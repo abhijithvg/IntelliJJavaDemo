be43c9c090193a61bbc189a77b8ceb48775f1c65
Abhijith <abhijith@test1.com>
2020-06-07 17:21:50 +0000

    ReqNo3: Function myMethod CONTENT added - multiplication of 6

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -1,7 +1,9 @@
 public class HelloDemo {
 
     void myMethod() {
-
+        for (int i=1; i<=10; i++) {
+            System.out.println(i * 6);
+        }
     }
 
     public static void main(String[] args) {
